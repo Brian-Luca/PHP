d32c9c414fcedd3a3556238adf45c15e01cb40d9
Brian-Luca <blukindc@gmail.com>
2019-06-25 00:40:32 +0100

    Exercicios basicos de PHP

---
+++ Aula04/Tipos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title>
+        Tipos de variáveis
+    </title>
+</head>
+<body>
+    <div>
+        <?php
+            $n = 4.5;
+            echo $n. "<br/>";
+            $no = (string) "Brian";
+            $n = 17;
+            echo $n. "<br/>";
+            echo $no;
+            $nome = " Luca";
+            $idade = 17;
+            echo $nome. " tem ". $idade. " anos!<br>";
+            $nom = "Maria";
+            $id = 13;
+            echo "$nom tem $id anos!";
+        ?>
+    </div>
+</body>
+</html>
+</DOCTYPE>
---
+++ Aula05/Operadores.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title>
+        Operadores Aritméticos
+    </title>
+</head>
+<body>
+<div>
+    <?php
+        $n1 = 3;
+        $n2 = 2;
+        echo "<h2>Valores Recebidos: $n1 e $n2</h2>";
+        $m = ($n1 + $n2) / 2;
+        echo "A soma vale ". ($n1+$n2);
+        echo "<br>A subtração vale ". ($n1-$n2);
+        echo "<br>A Multiplicação vale ". ($n1*$n2);
+        echo "<br>A Divisão vale ". ($n1/$n2);
+        echo "<br>O Modulo vale ". ($n1%$n2);
+        echo "<br>A Média vale $m";
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];
+        echo "<h2>Valores Recebidos: $n1 e $n2</h2>";
+        $m = ($n1 + $n2) / 2;
+        echo "A soma vale ". ($n1+$n2);
+        echo "<br>A subtração vale ". ($n1-$n2);
+        echo "<br>A Multiplicação vale ". ($n1*$n2);
+        echo "<br>A Divisão vale ". ($n1/$n2);
+        echo "<br>O Modulo vale ". ($n1%$n2);
+        echo "<br>A Média vale $m";
+    ?>
+</div
+</body>
+</html>
+</DOCTYPE>
